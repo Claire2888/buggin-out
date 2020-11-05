804f4eefe789370f14411aecbe537a1f65727d4b
Gavin Jacobs <69944182+Gavin-Jacobs@users.noreply.github.com>
2020-11-06 10:41:06 +1300

    home link done

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-
+ import Home from './home'
 
 const App = () => {
   return (
     <>
-      <h1>Navigating the taxonomic routes</h1>
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className='main'>
-        <Route path='/' component={Nav} />
-        <Route path='/list/:rank' component={Classifications} />
-        <Route path='/rank/:rank/:name' component={RankDescription} />
+        <Route path='/question' component={Home} /> 
+        {/* <Route path='/list/:rank' component={Classifications} />
+        <Route path='/rank/:rank/:name' component={RankDescription} /> */}
         <Route exact path='/' component={Home} />
       </div>
     </>
