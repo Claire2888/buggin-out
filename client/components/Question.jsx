62ac313a0061e211d44e155c14e45d160323b70e
Claire2888 <70086563+Claire2888@users.noreply.github.com>
2020-11-06 14:18:14 +1300

    applied styling to question route

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
 
 const Question = () => {
   return (
     <div>
-      <h1>Do you like scary movies?</h1>
-      <Link to='/question/Crawly'><button>yes</button></Link>
-      <Link to='/question/skritery'><button>no</button></Link>
+      <h1 className= 'font-question'>Do you like scary movies?</h1>
+      <Link to='/question/Crawly'>
+        <Button type='button' className='font-question, btn btn-primary-question btn-lg'>yes</Button></Link>
+      
+      <Link to='/question/skritery'>
+    <Button type='button' className='font-question, btn btn-primary-question btn-lg'>no</Button>
+    </Link>
     </div>
   )
 }
--- client/components/Question2.jsx
+++ client/components/Question2.jsx
@@ -5,6 +5,7 @@ const Question2 = () => {
   return (
     <div>
       <h1>Do you like to make things?</h1>
+      
       <Link to='/answer/Spider'><button>Yes</button></Link>
       <Link to='/answer/Beetle'><button>No</button></Link>
     </div>
