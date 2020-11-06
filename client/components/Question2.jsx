65e8b5a2bb7471abc869492b153f142c827f2cd3
tomgriffin <249769+tomgriffin@users.noreply.github.com>
2020-11-06 14:31:25 +1300

    applied more styling

--- client/components/Question2.jsx
+++ client/components/Question2.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
 
 const Question2 = () => {
   return (
     <div>
-      <h1>Do you like to make things?</h1>
-      
-      <Link to='/answer/Spider'><button>Yes</button></Link>
-      <Link to='/answer/Beetle'><button>No</button></Link>
+      <h1 className= 'font-question'>Do you like to make things?</h1>
+
+      <Link to='/answer/Spider'><Button type='button' className='font-question, btn btn-primary-question btn-lg'>yes</Button>
+      </Link>
+      <Link to='/answer/Beetle'><Button type='button' className='font-question, btn btn-primary-question btn-lg'>no</Button>
+      </Link>
     </div>
   )
 }
--- client/components/Question3.jsx
+++ client/components/Question3.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
 
 const Question3 = () => {
   return (
     <div>
-      <h1>Do you like to swim?</h1>
-      <Link to='/answer/Weta'><button>Yes</button></Link>
-      <Link to='/answer/Moth'><button>No</button></Link>
+      <h1 className='font-question'>Do you like to swim?</h1>
+      <Link to='/answer/Weta'><Button type='button' className='font-question, btn btn-primary-question btn-lg'>yes</Button></Link>
+      <Link to='/answer/Moth'><Button type='button' className='font-question, btn btn-primary-question btn-lg'>no</Button></Link>
     </div>
   )
 }
